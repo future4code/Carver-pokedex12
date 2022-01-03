58f51eacabe9f52beb958b964ca038df2a3c0a2c
0dayIg0r <igormedeiros.f@gmail.com>
2022-01-03 16:10:52 -0300

    add header + styled + logo

---
+++ pokedex/src/components/header/style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+import Toolbar from '@mui/material/Toolbar'
+
+
+export  const StyledToolbar = styled(Toolbar)`
+    background-color: #000000;
+    color: #F5EDED;
+`
+export const MenuContent = styled.div`
+background-color: #000;
+display: flex;
+align-items: center;
+width: 100vw;
+`
+export const PokedexIcon = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+img{
+    background-color: transparent;
+    width: 60px;
+    height: 60px;
+}
+&:hover{
+    img{
+    background-color: transparent;
+    width: 70px;
+    height: 70px;
+    cursor: pointer;
+    }
+}
+
+`
+export const Title = styled.div`
+display: flex;
+align-items: center;
+width: 100vw;
+&:hover{
+    color: #FE3D31;
+}
+`
