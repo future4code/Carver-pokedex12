4d0ce1d7f191b293d0f898c576117050cc094796
yMarceloMaia <marcelomaiajp@gmail.com>
2022-01-03 16:29:18 -0300

    Fazendo o Router

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/header/Header";
-
+import DetailsPage from "./pages/DetailsPage/DetailsPage";
+import HomePage from "./pages/HomePage/HomePage";
+import PokedexPage from "./pages/PokedexPage/PokedexPage";
+import Router from "./router/Router"
 
 function App() {
   return (
     <div >
-     <Header />
+      <Header />
+      <Router />
     </div>
   );
 }
