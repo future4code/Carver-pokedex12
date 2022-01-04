f47299f9448328802152472db404fbc06bfad407
0dayIg0r <igormedeiros.f@gmail.com>
2022-01-03 21:33:56 -0300

    criando page erro, função de navigate e usesate

--- pokedex/src/components/header/Header.js
+++ pokedex/src/components/header/Header.js
@@ -3,11 +3,30 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Logo from './img/Logo.png'
-
+import LogoPokedex from './img/LogoPokedex.png'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import * as C from './style'
 
 const Header = () => {
+  const navigate = useNavigate()
+  const [title, setTitle] = useState('Pokedex')
+  const [logo, setLogo] = useState(Logo)
+
+  function goPokedex(){
+    if(title === 'Pokedex'){
+      navigate('/pokedex')
+      setTitle('Lista de Pokemons')
+      setLogo(LogoPokedex)
+    } else{
+      navigate('/')
+      setTitle('Pokedex')
+      setLogo(Logo)
+    }
+  }
+  
+ 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -15,9 +34,16 @@ const Header = () => {
           <C.MenuContent>
 
            <C.PokedexIcon>
-           <img src={Logo}/>
-           <Button variant="string" style={{color: '#D72323'}} size='string'>Pokedex</Button>
+           <img src={logo}/>
+
+           <Button 
+           onClick={goPokedex}
+            variant="string" 
+           style={{color: '#D72323'}} 
+           size='string'>{title}
+           </Button>
            </C.PokedexIcon>
+
           </C.MenuContent>
           <C.Title>
             <h2>Lista de Pokemons</h2>
--- pokedex/src/pages/ErrorPage/ErrorPage.js
+++ pokedex/src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,26 @@
 import React from "react"
+import Error from '../../components/header/img/Error.gif'
+import BackHome from '../../components/header/img/BackHome.gif'
+import { useNavigate } from "react-router-dom"
+import * as C from './style'
 
 const ErrorPage = () => {
+    const navigate = useNavigate()
+
+    function goHome(){
+        navigate('/')
+    }
     return (
-        <div>
-            <h1>ErrorPage</h1>
-        </div>
+        <C.DivContent>
+            <div>
+                 <h1>Pagina não encontrada</h1>
+                 <h1>404!</h1>
+                 <C.ImgBackHome src={BackHome} onClick={goHome}/>
+                 <h3>Clique no Squirtle para voltar</h3>
+            </div>
+           
+            <C.ImgPika src={Error}/>
+        </C.DivContent>
     )
 }
 
